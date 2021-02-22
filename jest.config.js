e168b0e2ead599ae12f499cf9cd02bad1ae1b885
Otoniel Reyes Galay <otonielreyesgalay@gmail.com>
2021-02-22 07:10:01 -0400

    initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/grademy'],
+  projects: [
+    '<rootDir>/apps/grademy',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/data',
+    '<rootDir>/libs/ui',
+  ],
 };
